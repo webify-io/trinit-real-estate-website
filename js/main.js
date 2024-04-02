61d5f646de8088bac9f93c3e047fcb374614885d
webify-io <webify.projects@gmail.com>
2024-04-02 21:56:15 +0200

    Completed SCROLL SECTIONS ACTIVE LINK

--- js/main.js
+++ js/main.js
@@ -61,6 +61,27 @@ const toggleItem = (item) => {
 };
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+	const scrollDown = window.scrollY;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight,
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute('id'),
+			sectionsClass = document.querySelector(
+				'.nav-menu a[href*=' + sectionId + ']'
+			);
+
+		if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+			sectionsClass.classList.add('active-link');
+		} else {
+			sectionsClass.classList.remove('active-link');
+		}
+	});
+};
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
